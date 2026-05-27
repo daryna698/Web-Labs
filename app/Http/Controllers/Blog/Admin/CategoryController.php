f6dc68787b058da383e1a1489ee3c07c66a96f39
ilcuk <darynailcuk698@gmail.com>
2026-05-27 18:30:06 +0000

    feat: implement admin category controller, validation logic and API resources for lab 4

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+use App\Models\BlogCategory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $paginator = BlogCategory::paginate(5);
+
+        return $paginator;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $item = BlogCategory::create($data);
+
+        if ($item) {
+            return ['success' => 'Успішно збережено'];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $item = BlogCategory::find($id);
+
+        if (empty($item)) {
+            return back()
+            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"])
+            ->withInput();
+        }
+
+        $data = $request->all();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return ['success' => 'Успішно збережено'];
+        } else {
+            return ['msg' => 'Помилка збереження'];
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Blog\PostController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group([ 'namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
+    Route::apiResource('posts', PostController::class)->names('blog.posts');
+});
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','store','update',];
+    Route::apiResource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
