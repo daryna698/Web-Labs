7969fa692ee668c6d06b7d369345978f3689698e
ilcuk <darynailcuk698@gmail.com>
2026-06-02 10:13:39 +0000

    feat: implement blog post update API with validation.

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Api\Blog\Admin;
 
 use App\Repositories\BlogPostRepository;
 use Illuminate\Http\Request;
+use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogPostUpdateRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 
 class PostController extends BaseController
 {
     public function __construct(
-        private BlogPostRepository $blogPostRepository
-    ) {
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository // властивість через яку будемо звертатись в репозиторій категорій
+)
+    {
     }
 
     public function index()
@@ -24,9 +30,31 @@ class PostController extends BaseController
         //
     }
 
-    public function update(Request $request, string $id)
+    public function update(BlogPostUpdateRequest $request, string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+        if (empty($item->published_at) && $data['is_published']) { //якщо поле published_at порожнє і нам прийшло 1 в ключі is_published, то
+            $data['published_at'] = Carbon::now(); //генеруємо поточну дату
+        }
+        $result = $item->update($data); //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено'
+            ];
+        } else {
+            return ['message' => 'Помилка збереження'];
+        }
     }
 
     public function destroy(string $id)
