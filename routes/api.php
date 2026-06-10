065a2e952b1d28d738f4db3009619c2d30f8e961
ilcuk <darynailcuk698@gmail.com>
2026-06-10 20:58:28 +0000

    feat: implement job queues chaining and update dependencies

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Blog\Admin\CategoryController;
 use App\Http\Controllers\Api\Blog\Admin\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Api\Blog', 'prefix' => 'blog'], function () {
     Route::apiResource('posts', PostController::class)->names('blog.posts');
@@ -23,4 +24,12 @@ Route::group($groupData, function () {
     Route::apiResource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
+
+    Route::group(['prefix' => 'digging_deeper'], function () {
+        Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+            ->name('digging_deeper.processVideo');
+
+        Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+            ->name('digging_deeper.prepareCatalog');
+    });
 });
